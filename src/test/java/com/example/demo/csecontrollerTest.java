d48b0e8e3c48a7e1ba77faba17a7d6ac968ad676
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2026-03-01 12:40:38 +0530

    Update csecontrollerTest.java

--- src/test/java/com/example/demo/csecontrollerTest.java
+++ src/test/java/com/example/demo/csecontrollerTest.java
@@ -1,18 +1,24 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class csecontrollerTest {
-	@Autowired
-	csecontroller c;
-	@Test
-	void test() {
-		int result = c.addCSE(2, 3);
-		assertEquals(5, result);
-	}
+public class CseControllerTest {
 
+    @Autowired
+    CseController c;
+
+    @Test
+    void testAdditionWithRollNo() {
+        String expectedMessage = 
+            "After performing the addition, the final output is 30. My roll number is 23MH1A05N8.";
+
+        String actualMessage = c.addCSE(10, 20);
+
+        assertEquals(expectedMessage, actualMessage);
+    }
 }
