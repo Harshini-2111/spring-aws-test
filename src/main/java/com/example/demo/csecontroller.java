705aece3dbc9210e3a4321d8f73f8920579df6d0
Harshini Pechetti <23mh1a05o0@acoe.edu.in>
2026-03-01 12:41:13 +0530

    Update csecontroller.java

--- src/main/java/com/example/demo/csecontroller.java
+++ src/main/java/com/example/demo/csecontroller.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class csecontroller {
-	@GetMapping("/cseadd")
-	public int addCSE(@RequestParam int a, @RequestParam int b) {
-		return a + b;
-	}
+public class CseController {
+
+    @GetMapping("/cseadd")
+    public String addCSE(@RequestParam int a, @RequestParam int b) {
+        int result = a + b;
+        return "After performing the addition, the final output is " 
+                + result + ". My roll number is 23MH1A05N8.";
+    }
 }
